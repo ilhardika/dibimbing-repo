7bf3d60ea4ba02e475b90e8ba11022bf63744975
ilhardika <ilhardika67@gmail.com>
2024-11-16 13:01:40 +0700

    web cafe - change layout

--- Project/Cafe/tailwind.config.js
+++ Project/Cafe/tailwind.config.js
@@ -2,12 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    screens: {
-      sm: "340px",
-      md: "540px",
-      lg: "768px",
-      xl: "1180px",
-    },
+    // screens: {
+    //   sm: "340px",
+    //   md: "540px",
+    //   lg: "768px",
+    //   xl: "1180px",
+    // },
     extend: {
       colors: {
         bg: {
