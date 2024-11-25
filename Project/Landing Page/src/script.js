06ef5d53c4bb433dad42422ff75322034250c669
ilhardika <ilhardika67@gmail.com>
2024-11-25 21:59:15 +0700

    build landing page

---
+++ Project/Landing Page/src/script.js
@@ -0,0 +1,7 @@
+// Toggle mobile menu visibility
+const hamburger = document.getElementById("hamburger");
+const mobileMenu = document.getElementById("mobile-menu");
+
+hamburger.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
