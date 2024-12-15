5135cd2cc783f47760dafeb89ac9a235eac6f3e7
ilhardika <ilhardika67@gmail.com>
2024-12-15 08:42:13 +0700

    add detail page api

--- MyCode/8. React/react_6/src/pages/DetailPage/index.jsx
+++ MyCode/8. React/react_6/src/pages/DetailPage/index.jsx
@@ -20,22 +20,33 @@ const DetailPage = () => {
 
   return (
     <div>
-      <Navbar></Navbar>
+      <Navbar />
       <div className="m-10">
         <h1 className="text-2xl text-center font-bold">{detailRecipes.name}</h1>
-        <p>Ingredients : {detailRecipes.ingredients.join(", ")}</p>
-        {/* <div className="mb-2">
-          <p className="">Ingredients: </p>
-          {detailRecipes.ingredients.map((ingredient) => (
-            <p className="">{ingredient}</p>
-          ))}
-        </div> */}
-        <p>Prep Time Minutes: {detailRecipes.prepTimeMinutes}</p>
-        <p>Cook Time Minutes: {detailRecipes.cookTimeMinutes}</p>
-        <p>Servings: {detailRecipes.servings}</p>
-        <p>Difficulty: {detailRecipes.difficulty}</p>
-        <p>Cuisine: {detailRecipes.cuisine}</p>
-        <p>Calories: {detailRecipes.caloriesPerServing}</p>
+        <div className="flex flex-col gap-5">
+          <div>
+            <h2 className="text-xl font-semibold">Ingredients:</h2>
+            <ul className="list-disc ">
+              {detailRecipes?.ingredients?.map((ingredient) => (
+                <li className="ml-5">{ingredient}</li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <h2 className="text-xl font-semibold">Instructions:</h2>
+            <ul className="list-decimal">
+              {detailRecipes?.instructions?.map((instruction) => (
+                <li className="ml-5">{instruction}</li>
+              ))}
+            </ul>
+          </div>
+          <p>Prep Time Minutes: {detailRecipes.prepTimeMinutes}</p>
+          <p>Cook Time Minutes: {detailRecipes.cookTimeMinutes}</p>
+          <p>Servings: {detailRecipes.servings}</p>
+          <p>Difficulty: {detailRecipes.difficulty}</p>
+          <p>Cuisine: {detailRecipes.cuisine}</p>
+          <p>Calories: {detailRecipes.caloriesPerServing}</p>
+        </div>
       </div>
     </div>
   );
--- MyCode/8. React/react_6/src/pages/HomePage/index.jsx
+++ MyCode/8. React/react_6/src/pages/HomePage/index.jsx
@@ -20,28 +20,38 @@ const HomePage = () => {
   return (
     <div>
       <Navbar></Navbar>
-      <h1 className="text-2xl text-center font-bold mt-10">Recipes</h1>
+      <h1 className="text-2xl text-center font-bold mt-10">Recipes Lists</h1>
       {/* wrapper */}
-      <div className="mx-10 flex flex-col gap-10 my-10">
+      <div className="mx-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 my-10">
         {recipes.map((recipe) => (
           // card
           <div
             key={recipe.id}
-            className="border-2 border-gray-200 rounded-lg shadow-lg p-5"
+            className="border-2 border-gray-200 rounded-lg shadow-lg p-5 flex flex-col justify-between"
           >
-            <h2 className="my-1 font-bold">{recipe.name}</h2>
-            {/* {recipe?.ingredients.map((ingredient) => (
-              <p>{ingredient.name}</p>
-            ))} */}
-            <p className="my-1">Ingredients: {recipe.ingredients.join(", ")}</p>
-            <p className="my-1">Prep Time: {recipe.prepTimeMinutes} minutes</p>
-            <p className="my-1">Cook Time: {recipe.cookTimeMinutes} minutes</p>
-            <p className="my-1">Difficulty: {recipe.difficulty}</p>
-            <Link to={`/detail/${recipe.id}`}>
-              <button className="my-1 bg-gray-800  text-white font-bold py-2 px-5 rounded-lg text-sm">
-                Detail Recipe
-              </button>
-            </Link>
+            <div>
+              <h2 className="my-1 font-bold">{recipe.name}</h2>
+              <h2 className="my-1">Ingredients: </h2>
+              <ul className="list-disc ">
+                {recipe?.ingredients?.map((ingredient) => (
+                  <li className="ml-5">{ingredient}</li>
+                ))}
+              </ul>
+              <p className="my-1">
+                Prep Time: {recipe.prepTimeMinutes} minutes
+              </p>
+              <p className="my-1">
+                Cook Time: {recipe.cookTimeMinutes} minutes
+              </p>
+              <p className="my-1">Difficulty: {recipe.difficulty}</p>
+            </div>
+            <div>
+              <Link to={`/detail/${recipe.id}`}>
+                <button className="mt-5 bg-gray-800  text-white font-bold py-2 px-5 rounded-lg text-sm">
+                  Detail Recipe
+                </button>
+              </Link>
+            </div>
           </div>
         ))}
       </div>
