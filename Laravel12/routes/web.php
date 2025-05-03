92df1cb3e848ea979f9842a2af9da193b1b1336d
ilhardika <ilhardika67@gmail.com>
2025-05-03 15:38:43 +0700

    Add new pages and update routes for home, about, users, and user details
    - Updated the home route to return a view with a title.
    - Added a new route for the about page using Route::view.
    - Created a route for displaying a list of users.
    - Implemented a route to show user details based on an ID parameter.
    - Added Blade templates for home, about, users, and user detail pages.

--- Laravel12/routes/web.php
+++ Laravel12/routes/web.php
@@ -3,5 +3,20 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $data = [
+        'title' => 'Home',
+    ];
+    return view('pages.home', $data);
+});
+
+// bisa pakai view, / ganti .
+Route::view('/about', 'pages.about');
+
+Route::get('user/', function() {
+    return view('pages.users');
+});
+
+// menangkap parameter id dan mengirimkannya ke view
+Route::get('user/{id}', function($id) {
+    return view('pages.userDetail', ['id' => $id]);
 });
