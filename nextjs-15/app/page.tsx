f22c385f732574a0144ce56e1792ce97486a87e4
ilhardika <ilhardika67@gmail.com>
2025-05-10 21:40:45 +0700

    feat: add database.json with initial posts, comments, and profile data

--- nextjs-15/app/page.tsx
+++ nextjs-15/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Counter } from "./counter";
 
 export default function Home() {
@@ -5,6 +6,12 @@ export default function Home() {
     <>
       <h1>Home</h1>
       <Counter />
+      <Image
+        src="https://images.unsplash.com/photo-1745949779026-f7fdd1470f8c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+        width={300}
+        height={300}
+        alt="gambar 1"
+      />
     </>
   );
 }
